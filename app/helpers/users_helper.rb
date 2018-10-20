a4309ae79c6826d74927a0de17891a3bb71adea1
UsmanAzmat300 <d.usmanazmat@gmil.com>
2018-10-20 17:52:50 +0500

    Finish user signup

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,16 @@
 module UsersHelper
+
+def gravatar_for(user , size: 28   )
+  gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+
+  # size = options[:size]
+  gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+
+
+  # gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+  image_tag(gravatar_url, alt: user.name, class: "gravatar")
+
+end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,12 @@ Rails.application.routes.draw do
   get '/about', to: "static_pages#about"
   get '/contact', to: "static_pages#contact"
   get  '/signup',  to: 'users#new'
-
+  post "/signup", to: 'users#create'
 
   #get 'static_pages/help'
   #get 'static_pages/about'
   #get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-
+    resources :users
 end

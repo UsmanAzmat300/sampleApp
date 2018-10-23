5fb67a974102832239a0301f9f179742b5ba764c
UsmanAzmat300 <d.usmanazmat@gmil.com>
2018-10-23 18:04:46 +0500

    gi

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,6 +4,8 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   test "layout links" do
     get root_path
+
+
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", helf_path
